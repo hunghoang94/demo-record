9188d2d27393df2361234328445dd2f69d9907ba
HungHoang <hung.hoangvu@nttdata.com>
2020-09-04 17:28:19 +0700

    add new lib to test record

--- src/components/recorder/recorder.js
+++ src/components/recorder/recorder.js
@@ -5,8 +5,17 @@ import CallDetectorManager from 'react-native-call-detection';
 import AudioRecord from 'react-native-audio-record';
 import * as Contacts from 'react-native-contacts';
 
+import AudioRecorderPlayer,  {
+AVEncoderAudioQualityIOSType,
+AVEncodingOption,
+AudioEncoderAndroidType,
+AudioSet,
+AudioSourceAndroidType,
+} from 'react-native-audio-recorder-player';
+
 export default Recorder = () => {
   let [callDetector, setCallDetector] = useState(undefined);
+  let [audioRecorderPlayer , setAudioRecorderPlayer ] = useState(undefined);
   let [isStart, setIsStart] = useState(false);
 
   const initAudioRecord = async () => {
@@ -21,14 +30,15 @@ export default Recorder = () => {
     );
 
     if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-      const recordOptions = {
-        sampleRate: 16000,
-        channels: 1,
-        bitsPerSample: 16,
-        audioSource: 6,
-        wavFile: 'phone-record.wav'
-      };
-      AudioRecord.init(recordOptions);
+      // const recordOptions = {
+      //   sampleRate: 16000,
+      //   channels: 1,
+      //   bitsPerSample: 16,
+      //   audioSource: 6,
+      //   wavFile: 'phone-record.wav'
+      // };
+      // AudioRecord.init(recordOptions);
+      setAudioRecorderPlayer(new AudioRecorderPlayer());
     }
   };
 
@@ -37,30 +47,53 @@ export default Recorder = () => {
     initAudioRecord();
   }, []);
 
-  const onPress = () => {
-    if (isStart) {
-      if (callDetector) {
-          callDetector.dispose();
-      }
+  const onPress = async () => {
+    // if (isStart) {
+    //   if (callDetector) {
+    //       callDetector.dispose();
+    //   }
+    // } else {
+    //   setCallDetector(new CallDetectorManager((event, phoneNumber) => {
+    //       console.log(event);
+    //
+    //       if (event === 'Disconnected') {
+    //         AudioRecord.stop();
+    //       }
+    //       else if (event === 'Connected') {
+    //         AudioRecord.start();
+    //       }
+    //       else if (event === 'Offhook') {
+    //         AudioRecord.start();
+    //       }
+    //     }, false, () => {
+    //     }, {
+    //       title: 'Phone State Permission',
+    //       message: 'This app needs access to your phone state in order to react and/or to adapt to incoming calls.',
+    //     },
+    //   ));
+    // }
+    if (!isStart) {
+      // AudioRecord.start();
+      const path = 'hello.m4a';
+      const audioSet = {
+        AudioEncoderAndroid: AudioEncoderAndroidType.AAC,
+        AudioSourceAndroid: AudioSourceAndroidType.MIC,
+        AVEncoderAudioQualityKeyIOS: AVEncoderAudioQualityIOSType.high,
+        AVNumberOfChannelsKeyIOS: 2,
+        AVFormatIDKeyIOS: AVEncodingOption.aac,
+      };
+      const uri = await audioRecorderPlayer.startRecorder(path, audioSet);
+      audioRecorderPlayer.addRecordBackListener((e) => {
+        console.log(e)
+      })
+      console.log(uri)
     } else {
-      setCallDetector(new CallDetectorManager((event, phoneNumber) => {
-          console.log(event);
-
-          if (event === 'Disconnected') {
-            AudioRecord.stop();
-          }
-          else if (event === 'Connected') {
-            AudioRecord.start();
-          }
-          else if (event === 'Offhook') {
-            AudioRecord.start();
-          }
-        }, false, () => {
-        }, {
-          title: 'Phone State Permission',
-          message: 'This app needs access to your phone state in order to react and/or to adapt to incoming calls.',
-        },
-      ));
+      // AudioRecord.stop().then((res) => {
+      //   console.log(res)
+      // });
+      const result = await audioRecorderPlayer.stopRecorder();
+      audioRecorderPlayer.removeRecordBackListener();
+      console.log(result);
     }
     setIsStart(!isStart);
   };
